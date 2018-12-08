96c0d2d5a09117f28592aedba6e95f485f2f4a53
Spyrivasi <43912901+Spyrivasi@users.noreply.github.com>
2018-12-08 16:10:56 +0200

    Create showHideScripts.js

---
+++ assets/scripts/showHideScripts.js
@@ -0,0 +1,47 @@
+  
+/* Script by: www.jtricks.com 
+ * Version: 20090221 
+ * Latest version: 
+ * www.jtricks.com/javascript/blocks/showinghiding.html 
+ */  
+ 
+function showHide(what) {
+  if (what == "top") {
+    showPageElement("pie-chart);
+    showPageElement("tree-form);
+    showPageElement("stack-form);
+    return false;
+  }
+  
+  
+}
+function showPageElement(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    obj.style.display = 'block';  
+    return false;  
+}  
+  
+function hidePageElement(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    obj.style.display = 'none';  
+    return false;  
+}  
+  
+function togglePageElementVisibility(what)  
+{  
+    var obj = typeof what == 'object'  
+        ? what : document.getElementById(what);  
+  
+    if (obj.style.display == 'none')  
+        obj.style.display = 'block';  
+    else  
+        obj.style.display = 'none';  
+    return false;  
+}  
+  
